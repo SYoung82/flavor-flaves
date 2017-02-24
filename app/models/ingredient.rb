07d7907329d8f7783cc01b594376386cdbbfd8e5
Scott Young <young.scottw@gmail.com>
2017-02-24 11:22:25 -0600

    Added validations to Ingredient and Recipe models

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,5 +1,6 @@
 class Ingredient < ApplicationRecord
+  validates :name, uniqueness: true
 
+  has_many :recipe_ingredients
   has_many :recipes, through: :recipe_ingredients
-
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,6 @@
 class Recipe < ApplicationRecord
+  validates :title, uniqueness: true
 
+  has_many :recipe_ingredients
   has_many :ingredients, through: :recipe_ingredients
-
 end
