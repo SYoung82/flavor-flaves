4cdef7844ac7bee90cb07749d8262a137d1ee992
Scott Young <young.scottw@gmail.com>
2017-02-24 01:27:21 -0600

    Added ingredient model and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170224062023) do
+ActiveRecord::Schema.define(version: 20170224072641) do
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
