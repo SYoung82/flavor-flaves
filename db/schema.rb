737d8bf4bfaf13a98b4fde7909ef53c4fddd91d5
Scott Young <young.scottw@gmail.com>
2017-02-24 10:57:32 -0600

    Added recipe_ingredient join table

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170224072755) do
+ActiveRecord::Schema.define(version: 20170224165700) do
 
   create_table "ingredients", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "recipe_ingredients", force: :cascade do |t|
+    t.integer "recipe_id"
+    t.integer "ingredient_id"
   end
 
   create_table "recipes", force: :cascade do |t|
