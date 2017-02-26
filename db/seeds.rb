7ce2aeb73e9daf2ef72a326cf5b4383e3f9d8a34
Scott Young <young.scottw@gmail.com>
2017-02-26 11:15:07 -0600

    Populated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+ingredient1 = Ingredient.create(name: "Vegetable Oil", measurement: "tablespoon")
+ingredient2 = Ingredient.create(name: "Brocollie Florets", measurement: "cup")
+ingredient3 = Ingredient.create(name: "Bell Pepper", measurement: "")
+ingredient4 = Ingredient.create(name: "Carrot", measurement: "")
+ingredient5 = Ingredient.create(name: "Soy Sauce", measurement: "tablespoon")
+ingredient6 = Ingredient.create(name: "Minced Garlic", measurement: "teaspoon")
+ingredient7 = Ingredient.create(name: "Ground Beef", measurement: "lb")
+ingredient8 = Ingredient.create(name: "Cheddar Cheese", measurement: "slice")
+ingredient9 = Ingredient.create(name: "Tomato", measurement: "")
+ingredient10 = Ingredient.create(name: "Lettuce", measurement: "leaf")
+ingredient11 = Ingredient.create(name: "Bun", measurement: "")
+
+recipe1 = Recipe.create(title: "Vegetable Stir Fry", directions: "Heat vegetable oil in a large wok or skillet over
+                                                                  medium heat.  Add brocolli, bell pepper, carrot, and garlic.
+                                                                  Cook and stir for 2 minutes.  Season with soy sauce.  Cook
+                                                                  until vegetables are tender.  Emjoy.")
+
+recipe2 = Recipe.create(title: "Cheeseburgers", directions: "Heat grill to medium.  Seperate ground beef into 4 equal portions
+                                                             and form patties.  While grill is heating slice tomatos and Seperate
+                                                             and clean 4 lettuce leaves.  Grill burger patties until browned then flip
+                                                             and repeat.  Place burger patties on toasted buns and top with tomato,
+                                                             lettuce, cheese, and any other desired condiments.")
+
+recipe_ingredient1 = RecipeIngredient.create(recipe_id: 1, ingredient_id: 1)
+recipe_ingredient2 = RecipeIngredient.create(recipe_id: 1, ingredient_id: 2)
+recipe_ingredient3 = RecipeIngredient.create(recipe_id: 1, ingredient_id: 3)
+recipe_ingredient4 = RecipeIngredient.create(recipe_id: 1, ingredient_id: 4)
+recipe_ingredient5 = RecipeIngredient.create(recipe_id: 1, ingredient_id: 5)
+recipe_ingredient6 = RecipeIngredient.create(recipe_id: 1, ingredient_id: 6)
+recipe_ingredient7 = RecipeIngredient.create(recipe_id: 2, ingredient_id: 7)
+recipe_ingredient8 = RecipeIngredient.create(recipe_id: 2, ingredient_id: 8)
+recipe_ingredient9 = RecipeIngredient.create(recipe_id: 2, ingredient_id: 9)
+recipe_ingredient10 = RecipeIngredient.create(recipe_id: 2, ingredient_id: 10)
+recipe_ingredient11 = RecipeIngredient.create(recipe_id: 2, ingredient_id: 11)
