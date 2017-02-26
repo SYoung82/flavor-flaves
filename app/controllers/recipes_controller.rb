a340bbf67517b969c4f1f33b7a840f23950fde56
Scott Young <young.scottw@gmail.com>
2017-02-26 11:42:51 -0600

    Added errors to new recipe form, built RecipesController#recipe_params

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -8,4 +8,19 @@ class RecipesController < ApplicationController
   def new
     @recipe = Recipe.new
   end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title, :directions, :ingredients)
+  end
 end
