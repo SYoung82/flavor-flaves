627db9bc4b7aecf4d8b87801d4dd5356b30a961a
Scott Young <young.scottw@gmail.com>
2017-02-27 14:07:29 -0600

    Added RecipeController #edit and #update methods, built recipe edit view

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -26,6 +26,19 @@ class RecipesController < ApplicationController
     end
   end
 
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+    if @recipe.update(recipe_params)
+      redirect_to recipe_path(@recipe)
+    else
+      render :edit
+    end
+  end
+
   private
 
   def recipe_params
