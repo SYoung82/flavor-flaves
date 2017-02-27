376993bf2c7311f5985208ed8ebb8f630585f32d
Scott Young <young.scottw@gmail.com>
2017-02-27 10:32:42 -0600

    Fixed invalid saving of blank ingredient fields

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -7,11 +7,9 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
-    5.times { @recipe.ingredients.build }
   end
 
   def create
-    raise params.inspect
     @recipe = Recipe.new(recipe_params)
     if @recipe.save
       redirect_to root_path
@@ -23,6 +21,6 @@ class RecipesController < ApplicationController
   private
 
   def recipe_params
-    params.require(:recipe).permit(:title, :directions, :ingredient_ids, :ingredients)
+    params.require(:recipe).permit(:title, :directions, ingredient_ids:[], ingredients_attributes: [:name])
   end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,18 @@
 class Recipe < ApplicationRecord
   validates :title, uniqueness: true
   validates :title, :directions, presence: true
-  
+
   has_many :recipe_ingredients
   has_many :ingredients, through: :recipe_ingredients
+
+  accepts_nested_attributes_for :ingredients, allow_destroy: true
+
+  def ingredients_attributes=(ingredient_attributes)
+    ingredient_attributes.values.each do |ingredient_attribute|
+      ingredient = Ingredient.find_or_create_by(ingredient_attribute)
+      if ingredient[:name] != ''
+        self.ingredients << ingredient
+      end
+    end
+  end
 end
