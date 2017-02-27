ce6e099acfd18efff9b892585291d103b07a25ae
Scott Young <young.scottw@gmail.com>
2017-02-27 11:15:52 -0600

    Added recipe show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "recipes#index"
+  resources :recipes
+
+  root "recipes#index"
 
   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
 
   get "/auth/:provider/callback" => "recipes#index"
-
-  resource :recipes
 end
