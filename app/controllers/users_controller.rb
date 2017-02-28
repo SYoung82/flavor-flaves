7d214209cb6b7993ec71269c990d4264f0e4f605
Scott Young <young.scottw@gmail.com>
2017-02-28 13:31:48 -0600

    fixed users controller to only allow user to view his/her show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,17 @@
 class UsersController < ApplicationController
+  before_filter :user_is_current_user
+
   def show
     @user = User.find(params[:id])
     @recipes = @user.recipes
+  end
+
+  private
+
+  def user_is_current_user
+    unless current_user.id == params[:user_id]
+      flash[:notice] = "You may only view your page."
+      redirect_to root_path
+    end
   end
 end
