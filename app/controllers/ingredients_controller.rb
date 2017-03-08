ff9e295fa50d1936b8186b9cbfbc5de7d1d0c0d6
Scott Young <young.scottw@gmail.com>
2017-03-08 08:30:47 -0600

    Fixed edit ingredient behavior for ingredients not in recipe

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -12,12 +12,16 @@ class IngredientsController < ApplicationController
     @recipe = Recipe.find(params[:recipe_id])
     @ingredient = Ingredient.find(params[:id])
     @recipe_ingredient = RecipeIngredient.find_by(recipe_id: @recipe.id, ingredient_id: @ingredient.id)
+    if !@recipe_ingredient
+      redirect_to recipe_ingredients_path(recipe_id: params[:recipe_id])
+    end
   end
 
   def update
     @recipe_ingredient = RecipeIngredient.find_by(recipe_id: params[:recipe_id], ingredient_id: params[:id])
-    @recipe_ingredient.update(ingredient_params)
-    @recipe_ingredient.save
+    if @recipe_ingredient && @recipe_ingredient.update(ingredient_params)
+      @recipe_ingredient.save
+    end
     redirect_to recipe_ingredients_path(recipe_id: params[:recipe_id])
   end
 
