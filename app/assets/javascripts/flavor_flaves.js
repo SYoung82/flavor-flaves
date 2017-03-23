cb670525b90886577d29a7a304fe2140078797fe
Scott Young <young.scottw@gmail.com>
2017-03-23 11:11:01 -0500

    Added listeners for saved recipes

--- app/assets/javascripts/flavor_flaves.js
+++ app/assets/javascripts/flavor_flaves.js
@@ -1,30 +1,39 @@
 var attachListeners = function() {
     //Attach listeners for ???
-    $("#home a").click(function(event) {
-        event.preventDefault();
-        alert("Home click");
-    });
+    // $("#home a").click(function(event) {
+    //     event.preventDefault();
+    //     alert("Home click");
+    // });
+    //
+    // $("#ingredients_filter_submit").click(function(event) {
+    //     event.preventDefault();
+    //     alert("Filter click");
+    // });
+    //
+    // $("#saved").click(function(event) {
+    //     event.preventDefault();
+    //     alert("Saved click");
+    // });
+    //
+    // $("#top5").click(function(event) {
+    //     event.preventDefault();
+    //     alert("Top 5 click");
+    // });
+    //
+    // $("#submitted").click(function(event) {
+    //     event.preventDefault();
+    //     alert("Submitted click");
+    // });
 
-    $("#ingredients_filter_submit").click(function(event) {
+    $("img[alt='Saved']").click(function(event) {
         event.preventDefault();
-        alert("Filter click");
-    });
+        alert("Star clicked");
+    })
 
-    $("#saved").click(function(event) {
+    $("img[alt='Unsaved']").click(function(event) {
         event.preventDefault();
-        alert("Saved click");
-    });
-
-    $("#top5").click(function(event) {
-        event.preventDefault();
-        alert("Top 5 click");
-    });
-
-    $("#submitted").click(function(event) {
-        event.preventDefault();
-        alert("Submitted click");
-    });
-
+        alert("Empty star clicked");
+    })
 }
 
 $(function() {
