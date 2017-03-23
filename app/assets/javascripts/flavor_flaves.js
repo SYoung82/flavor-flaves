51e9957134817204b3593a0cc286ba3710595071
Scott Young <young.scottw@gmail.com>
2017-03-23 08:16:04 -0500

    Beautified several files

--- app/assets/javascripts/flavor_flaves.js
+++ app/assets/javascripts/flavor_flaves.js
@@ -1,31 +1,32 @@
 var attachListeners = function() {
-  //Attach listeners for ???
-   $("#home a").click(function(event){
-     event.preventDefault();
-     alert("Home click");
-   });
+    //Attach listeners for ???
+    $("#home a").click(function(event) {
+        event.preventDefault();
+        alert("Home click");
+    });
 
-   $("#ingredients_filter_submit").click(function(event){
-     event.preventDefault();
-     alert("Filter click");
-   });
+    $("#ingredients_filter_submit").click(function(event) {
+        event.preventDefault();
+        alert("Filter click");
+    });
 
-   $("#saved").click(function(event){
-     event.preventDefault();
-     alert("Saved click");
-   });
+    $("#saved").click(function(event) {
+        event.preventDefault();
+        alert("Saved click");
+    });
 
-   $("#top5").click(function(event){
-     event.preventDefault();
-     alert("Top 5 click");
-   });
+    $("#top5").click(function(event) {
+        event.preventDefault();
+        alert("Top 5 click");
+    });
 
-   $("#submitted").click(function(event){
-     event.preventDefault();
-     alert("Submitted click");
-   });
+    $("#submitted").click(function(event) {
+        event.preventDefault();
+        alert("Submitted click");
+    });
+
 }
 
-$(function(){
-  attachListeners();
+$(function() {
+    attachListeners();
 });
