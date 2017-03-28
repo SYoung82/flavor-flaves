784c21360132579b4c421ce19ccda6c73f6599d3
Scott Young <young.scottw@gmail.com>
2017-03-28 10:25:16 -0500

    Added user and recipe_ingredient serializers

--- app/serializers/ingredient_serializer.rb
+++ app/serializers/ingredient_serializer.rb
@@ -1,5 +1,5 @@
 class IngredientSerializer < ActiveModel::Serializer
   attributes :id, :name
-  # has_many :recipe_ingredients
-  # has_many :recipes, through: :recipe_ingredients
+  has_many :recipe_ingredients
+  has_many :ingredients, through: :recipe_ingredients
 end
