4f156e9b446f4b08cbdccba2efe4d526316dca3b
Scott Young <young.scottw@gmail.com>
2017-03-29 16:14:37 -0500

    Built ajax new recipe submission

--- app/assets/javascripts/recipe.js
+++ app/assets/javascripts/recipe.js
@@ -1,4 +1,18 @@
 function Recipe(attributes){
     this.title = attributes.title;
     this.directions = attributes.directions;
+    this.user_id = attributes.user_id;
 }
+
+$(function(){
+    $("form#new_recipe").on("submit", function(event){
+        event.preventDefault();
+
+        var $form = $(this);
+        var action = $form.attr("action");
+        var params = $form.serialize();
+
+        debugger;
+        $.post(action, params)
+    })
+})
