7ea7c9a107c0395ce0d0c9086a160654a8d864d9
Scott Young <young.scottw@gmail.com>
2017-03-30 13:21:57 -0500

    Reverted recipe.rb, discareded ingredient_quantity.rb

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,6 +4,7 @@ class Recipe < ApplicationRecord
 
   has_many :recipe_ingredients
   has_many :ingredients, through: :recipe_ingredients, dependent: :destroy
+  
   has_many :user_recipes
   has_many :users, through: :user_recipes, dependent: :destroy
 
